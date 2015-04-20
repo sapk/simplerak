e1d2f018d579e405cdd937d5d5176666289ab6f4
Antoine GIRARD <sapk@sapk.fr>
2015-04-20 12:40:05 +0200

    Add logic to the today button

--- res/js/simplerak.page.js
+++ res/js/simplerak.page.js
@@ -4,19 +4,24 @@ if (typeof S === 'undefined') {
 
 
 S.page = {
-        pos: 0,
-        today:0,
-        list: {},
-        goTo : function (n, duration){
-            S.page.pos = n;
-            $('#container').animate({scrollLeft: n * $(document).width() + "px"}, duration);
-        },
-        goRight : function (){
-            S.page.pos = (++S.page.pos > S.page.list.length) ? S.page.list.length : S.page.pos
-            S.page.goTo(S.page.pos,150)
-        },
-        goLeft : function (){
-            S.page.pos = (--S.page.pos < 0) ? 0 : S.page.pos
-            S.page.goTo(S.page.pos,150)
-        }
+    pos: 0,
+    today: 0,
+    list: {},
+    goTo: function (n, duration) {
+        S.page.pos = n;
+        $('#container').animate({scrollLeft: n * $(document).width() + "px"}, duration);
+        //console.log([S.page.pos ,S.page.today])
+        if (S.page.pos == S.page.today)
+            $("#go_today").parent().addClass('active');
+        else
+            $("#go_today").parent().removeClass('active');
+    },
+    goRight: function () {
+        S.page.pos = (++S.page.pos > S.page.list.length) ? S.page.list.length : S.page.pos
+        S.page.goTo(S.page.pos, 150)
+    },
+    goLeft: function () {
+        S.page.pos = (--S.page.pos < 0) ? 0 : S.page.pos
+        S.page.goTo(S.page.pos, 150)
+    }
 }
