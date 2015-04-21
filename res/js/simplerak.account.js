f45b58aa1b281f36b4b81da2ccab96045bff74ba
Antoine GIRARD <sapk@sapk.fr>
2015-04-22 01:16:29 +0200

    Implementing paiement

--- res/js/simplerak.account.js
+++ res/js/simplerak.account.js
@@ -43,6 +43,7 @@ S.account = {
                 "xajaxargs[]" : "<xjxquery><q>" + Object.keys(form)[0] + "=" + num + ".00&pdav0=0.00%20%E2%82%AC&pdav1=50.00%20%E2%82%AC%20&pdav2=60.00%20%E2%82%AC%20&pdav3=70.00%20%E2%82%AC%20&pdav4=80.00%20%E2%82%AC%20&pdav5=90.00%20%E2%82%AC%20&pdav6=100.00%20%E2%82%AC%20</q></xjxquery>"
             }
             alert(JSON.stringify(S.account.getForm(d, "form")))
+            alert(JSON.stringify(data))
             //TODO 
             if (S.app.isWebBrowser()) {
                 $.get("test/file/portemonnaie-rechargement-reponse.html", function (d) {
