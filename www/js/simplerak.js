b19c832b5dc7b141fe21e71299ea789b1ab85c13
Antoine GIRARD <sapk@sapk.fr>
2015-04-29 03:03:45 +0200

    Reorganisation of the repo for local building

---
+++ www/js/simplerak.js
@@ -0,0 +1,37 @@
+var S = {
+    config: {
+        default_language: "en"
+    },
+    init: function () {
+
+        var language = localStorage.userLanguage || window.navigator.userLanguage || window.navigator.language || navigator.browserLanguage || navigator.systemLanguage || S.config.default_language;
+        //TEST 
+        //language = "en-US"
+        language = language.split("-")[0];
+        if ($.inArray(language, ["en", "fr"]) === -1)
+            language = S.config.default_language;
+
+        console.log(language);
+        html10n.localize(language);
+
+        S.attach_event();
+
+        if (localStorage.theme)
+            $("body").addClass("theme-" + localStorage.theme);
+
+    },
+    app: {
+        isWebBrowser: function () {
+            return !(document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1);
+        }
+    },
+    attach_event: function () {
+        //Le menu de navigation se ferme au clic lorsque l'on est sur  mobile mais pas sur tablette
+        $("nav #show_menu").sideNav({closeOnClick: !($(document).width() > 992)});
+        document.addEventListener("menubutton", function () {
+            $("nav #show_menu").sideNav('show');
+        }, false);
+    }
+};
+
+$(S.init);
--- www/js/simplerak.menu.js
+++ www/js/simplerak.menu.js
@@ -13,14 +13,22 @@ S.menu = {
             S.menu.config.json_url = "test/file/menu.json";
         }
         //TODO
-        S.attach_event.menu();
+        S.menu.attach_event();
         $.get(S.menu.config.json_url, S.menu.parse, "json");
     },
+    attach_event: function () {
+        $("#container").hammer()
+        $("#container").on("swipeleft", S.page.goRight);
+        $("#container").on("swiperight", S.page.goLeft);
+        window.addEventListener("orientationchange", function () {
+            window.setTimeout("S.page.goTo(S.page.pos,300)", 100);
+        }, true);
+    },
     parse: function (d) {
-        //console.log(d);
-        // ON ajoute à S.page.list de manière ordonné
-        $(Object.keys(d).sort()).each(function (i,key) {
-            //console.log(key);
+//console.log(d);
+// ON ajoute à S.page.list de manière ordonné
+        $(Object.keys(d).sort()).each(function (i, key) {
+//console.log(key);
             S.page.list[key] = d[key];
         });
         //S.page.list = d.sort();
@@ -38,7 +46,6 @@ S.menu = {
 
         $(".progress").hide()
         $('.collapsible').collapsible();
-
         var day = (new Date()).toJSON().split("T")[0];
         S.page.today = parseInt($(".page[data-date='" + day + "']>h5").css("color", "#ef5350").css("font-weight", "bold").parent().attr('id'))
         S.page.goTo(S.page.today, 0);
